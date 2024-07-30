ed3b6fe2b7e5810781b71182788fcbb452ed36a4
Amogh.Kharche <Amogh.Kharche@DELL1536>
2024-07-30 14:15:45 +0530

    Add project files.

---
+++ AccountApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AccountApp;
+using AccountManagerSpace;
+using AccountProject;
+using AccountApp.Model;
+using AccountLibrary;
+
+namespace AccountProject
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            AccountManager.RoleSelection();
+            SerialDeserial.SerializeData(AccountManager.accounts);
+        }
+    }
+}
